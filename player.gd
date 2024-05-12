32e25c6b8276ab85cbfca1620f1de3df9c331672
[peeryo] <peeryo@sfusd.edu>
2024-05-11 18:27:06 -0700

    added walking and idle animation and created game scene

--- player.gd
+++ player.gd
@@ -4,4 +4,10 @@ func  _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right","move_up", "move_down")
 	velocity = direction * 600
 	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		%HappyBoo.play_walk_animation()
+	else:
+		%HappyBoo.play_idle_animation()
+		
 
